2c376f884db08fc7580ac481a06fff71ec1b2d7f
Sebastian <sebastianstrzalkowski.strzala@gmail.com>
2025-03-04 19:59:53 +0100

    Fix solhint errors and warnings

--- contracts/LiqfinityToken.sol
+++ contracts/LiqfinityToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /**
  * @title Liqfinity Token (LFAI)
@@ -13,6 +13,13 @@ contract LiqfinityToken is ERC20, ERC20Burnable, Ownable {
 
     // Constant defining the total token supply: 1 billion
     uint256 public constant INITIAL_SUPPLY = 1_000_000_000 * 10**18;
+
+    // Custom errors
+    error ZeroAddress();
+    error ZeroAmount();
+    error InsufficientTokenBalance();
+    error InsufficientETHBalance();
+    error ETHTransferFailed();
 
     /**
      * @dev Constructor that sets the name, symbol and allocates the initial amount of tokens to the deployer
@@ -28,10 +35,10 @@ contract LiqfinityToken is ERC20, ERC20Burnable, Ownable {
      * @param amount Amount of tokens to send
      */
     function withdrawTokens(address to, uint256 amount) public onlyOwner {
-        require(to != address(0), "LiqfinityToken: cannot withdraw to zero address");
-        require(amount > 0, "LiqfinityToken: amount must be greater than zero");
+        if (to == address(0)) revert ZeroAddress();
+        if (amount == 0) revert ZeroAmount();
 
-        require(balanceOf(address(this)) >= amount, "LiqfinityToken: insufficient token balance");
+        if (balanceOf(address(this)) < amount) revert InsufficientTokenBalance();
 
         _transfer(address(this), to, amount);
 
@@ -44,12 +51,12 @@ contract LiqfinityToken is ERC20, ERC20Burnable, Ownable {
      * @param amount Amount of ETH to send
      */
     function withdrawETH(address payable to, uint256 amount) public onlyOwner {
-        require(to != address(0), "LiqfinityToken: cannot withdraw to zero address");
-        require(amount > 0, "LiqfinityToken: amount must be greater than zero");
-        require(address(this).balance >= amount, "LiqfinityToken: insufficient ETH balance");
+        if (to == address(0)) revert ZeroAddress();
+        if (amount == 0) revert ZeroAmount();
+        if (address(this).balance < amount) revert InsufficientETHBalance();
 
         (bool success, ) = to.call{value: amount}("");
-        require(success, "LiqfinityToken: ETH transfer failed");
+        if (!success) revert ETHTransferFailed();
 
         emit ETHWithdrawn(to, amount);
     }
@@ -59,6 +66,44 @@ contract LiqfinityToken is ERC20, ERC20Burnable, Ownable {
      */
     receive() external payable {
         emit ETHReceived(msg.sender, msg.value);
+    }
+
+    /**
+     * @dev Increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * @param spender The address which will spend the funds
+     * @param addedValue The amount of tokens to increase the allowance by
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, allowance(owner, spender) + addedValue);
+        return true;
+    }
+
+    /**
+     * @dev Decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * @param spender The address which will spend the funds
+     * @param subtractedValue The amount of tokens to decrease the allowance by
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        address owner = _msgSender();
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance < subtractedValue) revert InsufficientTokenBalance();
+        unchecked {
+            _approve(owner, spender, currentAllowance - subtractedValue);
+        }
+        return true;
     }
 
     // Events
